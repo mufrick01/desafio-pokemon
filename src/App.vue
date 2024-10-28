52e58a31d65211103dec9713dcfd24ed470b2f46
mufrick01 <mrmufrick@gmail.com>
2024-10-28 20:30:21 -0300

    funcionalidad lista

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,55 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <h1 class="text-center">¿Quién es ese Pokémon?</h1>
+  <div id="app" class="container">
+    <div class="row">
+      <PokeCard  v-for="(pokemon, index) in pokeList"   :key="`poke-${index}`"     :pokeName="pokemon.name" :pokeNumber="pokemon.number" class="col-6 col-lg-3" />
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PokeCard from './PokeCard.vue';
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PokeCard
+  },
+  data(){
+    return{
+      pokeList:[]
+    }
+  },
+  created(){
+    try {
+      this.HitApi();
+    } catch (error) {
+      throw Error(error);
+    }
+  },
+
+  methods:{
+    async HitApi(){
+      const req = await fetch('https://pokeapi.co/api/v2/pokemon?limit=151');
+      const resp = await req.json();
+
+      
+      this.pokeList = resp.results.map((pkm)=>{
+
+        const number = pkm.url.split('/').at(-2)
+        return{
+          name:pkm.name,
+          number,
+        }
+      })
+    }
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
   margin-top: 60px;
 }
 </style>
---
+++ src/PokeCard.vue
@@ -0,0 +1,56 @@
+<template>
+  <article>
+    <img :src="this.srcImg" :alt="`image del pokemon ${pokeName}`" :class="{'whoisthat':!correctName}">
+    <h4 v-if="correctName" >{{ pokeName }}</h4>
+    <div v-if="!correctName">
+      <form @submit.prevent="clickHandler">
+        <input v-model="inputText" type="text">
+        <button type="submit">click</button>
+      </form>
+    </div>
+  </article>
+</template>
+
+<script>
+export default {
+  name: 'PokeCard',
+  data() {
+    return {
+      srcImg :`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${this.pokeNumber}.png`,
+      correctName:false,
+      inputText:'',
+    };
+  },
+  props:{
+      pokeName:String,
+      pokeNumber:String,
+  },
+  methods:{
+    clickHandler(){
+      console.log(this.inputText);
+      if(this.inputText.trim().toLocaleLowerCase() !== this.pokeName.toLocaleLowerCase()){
+        return 
+      }
+
+      this.correctName=true;
+    }
+  }
+};
+</script>
+
+<style scoped>
+/* Estilos aquí */
+
+.whoisthat{
+  filter: brightness(0);
+}
+article{
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+}
+form{
+  display: flex;
+  flex-direction: column
+}
+</style>
